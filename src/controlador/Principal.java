a5d254597bb24bae3227ec12525f53c9479b32df
Edilson Cichon <edilsoncichon@hotmail.com>
2017-10-15 10:35:13 -0200

    Configuracao do Hibernate concluida, as classes sendo mapeadas para o banco com sucesso.

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -1,10 +1,14 @@
 package controlador;
 
+import dao.ConfigDAO;
 import javax.swing.JOptionPane;
 
 public class Principal {
 
     public static void main(String[] args) {
+        
+        new ConfigDAO();
+        
         JOptionPane.showMessageDialog(null, "Hello Wordl!");
     }
     
---
+++ src/dao/ConfigDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+import dao.hibernate.HibernateConfig;
+
+public class ConfigDAO {
+   
+    public ConfigDAO() {
+        new HibernateConfig();
+    }
+}
