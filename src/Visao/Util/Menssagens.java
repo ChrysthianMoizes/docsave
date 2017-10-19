cd328463afa69433eb4f7560840ae01fd1ac15c6
reida <reida@172.16.44.2>
2017-10-19 21:27:18 -0200

    refatoração da classe Erro para Menssagens

---
+++ src/Visao/Util/Menssagens.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Visao.Util;
+
+import controlador.CIPrincipal;
+import java.awt.Component;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author reida
+ */
+public class Menssagens {
+    CIPrincipal ctrlP;
+
+    public Menssagens(CIPrincipal ctrlP) {
+        this.ctrlP = ctrlP;
+    }
+    
+    public void validaCampos(Component componentePai,ArrayList<String> listaErro){
+        if(!listaErro.isEmpty()){
+            String itens = "";
+            for(String item:listaErro){
+                itens += item;
+                itens += ", ";
+            }
+            itens = itens.substring(0, (itens.length()-2));
+            JOptionPane.showMessageDialog(componentePai, "Os seguintes campos estão branco: "+itens,
+                    "Alerta! Campos em branco", JOptionPane.WARNING_MESSAGE);
+        }
+        
+    }
+}
